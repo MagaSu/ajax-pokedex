7597c7692417d54210c3dd297b16d9a650ed65d8
OneMadness <magamag268@gmail.com>
2021-10-15 16:32:35 +0200

    Added some animations, and solved bugs

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ const btn = document.getElementsByClassName("btn");
 const previous = document.getElementById("previous");
 const next = document.getElementById("next");
 const search = document.getElementById("search");
+const loadAnimation = document.getElementById("loading-animation");
 
 // Pokemon display section
 const content = document.getElementById("pokemon-content");
 const moves = document.getElementById("pokemon-moves");
 
 let autoPilot = true;
+let opacityOn = true;
 let currentPokemonId;
 
 const getPokemonData = async (query) => {
@@ -17,18 +19,24 @@ const getPokemonData = async (query) => {
   const response = await fetch(url);
 
   if (response.status == 404 || response.statusText == "Not found") {
-    document.getElementById("show-error").classList.remove("hidden");
+    document.getElementById("show-error").classList.remove("disabled");
+    content.innerHTML = "";
     return;
   }
 
   const pokemon = await response.json();
+
+  loadAnimation.remove();
+
   currentPokemonId = pokemon.id;
+  pokemonMoves = pokemon.moves.slice(0, 4);
 
   pokemonImg =
     pokemon.sprites.other.dream_world.front_default == null
       ? pokemon.sprites.other["official-artwork"].front_default
       : pokemon.sprites.other.dream_world.front_default;
 
+  document.getElementById("info-title").innerText = "Characteristic";
   document.getElementById("pokemon-name").innerText = pokemon.name;
   document.getElementById("pokemon-img").setAttribute("src", pokemonImg);
   document.getElementById(
@@ -47,6 +55,30 @@ const getPokemonData = async (query) => {
     "pokemon-height"
   ).innerText = `Height: ${pokemon.height}m`;
   document.getElementById("pokemon-id").innerText = `ID: ${pokemon.id}`;
+
+  document.getElementById("moves-title").innerText = "Moves";
+  moves.innerHTML = "";
+  pokemonMoves.map((p) => {
+    if (p < 0) {
+      console.log("No moves");
+    }
+    let li = document.createElement("li");
+    li.innerHTML = p.move.name;
+    moves.appendChild(li);
+  });
+  if (opacityOn) {
+    for (let i = 1; i < 100; i++) {
+      document.getElementById("pokemon-display").style.opacity = `${i}%`;
+      document.getElementById("search-section").style.opacity = `${i}%`;
+    }
+    setTimeout(() => {
+      document.getElementById("pokemon-display").classList.remove("hidden");
+    }, 2000);
+    opacityOn = false;
+    document.getElementById("search-section").classList.remove("hidden");
+    previous.classList.remove("disabled");
+    next.classList.remove("disabled");
+  }
 };
 
 const mode = setInterval(() => {
